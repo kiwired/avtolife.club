ddd452dfbebb2c8fb9a1e9ff4e1f63ebb37a661a
Maxim Doronin <maxim.doronin@itcode.pro>
2020-06-03 20:03:22 +0300

    deploy next.js to gh-pages

---
+++ out/_next/static/BXsD53DvG_UYf-NIt6wG0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/services/polishing":["static\u002Fcss\u002Fdf512d6b4509a1ee0302.css"]}}("static\u002Fcss\u002F7f98b9ecc61735a1562b.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1242dc4116011e29d222";
+/******/ 	var hotCurrentHash = "5e6ce8d39fa25dbfb0c1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
