5a9e255f3419798e8b9a29cadc6c57587f2459f0
Maxim D <maksimdoronin93@gmail.com>
2022-03-21 13:16:50 +0200

    deploy next.js to gh-pages

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "812b184040644ff6059d";
+/******/ 	var hotCurrentHash = "885f5c69cd8bc39a82cc";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
