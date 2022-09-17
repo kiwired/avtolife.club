5884f8d0b75b4df5466768a16f29b6200b894e2c
Maxim D <maksimdoronin93@gmail.com>
2022-09-17 14:09:13 +0200

    function update

--- functions/api/sms.ts
+++ functions/api/sms.ts
@@ -3,20 +3,19 @@ import { sendJson } from '../utils/sendJson'
 export const onRequestPost = async ({ request, env }) => {
 	try {
 
-		// const body = await request.json<{ to: string, text: string }>()
+		const body: { to: string, text: string } = JSON.parse(request.body)
 
-		// const res = await fetch('https://sms.ru/sms/send', {
-		// 	method: 'POST',
-		// 	headers: {
-		// 		'Content-Type': 'application/json'
-		// 	},
-		// 	body: JSON.stringify({ ...body, api_id: env.SMS_API, json: true })
-		// })
+		const res = await fetch('https://sms.ru/sms/send', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ ...body, api_id: env.SMS_API, json: true })
+		})
 
-		// const json = await res.json()
+		const json = await res.json()
 
-		// return sendJson(json)
-		return sendJson({})
+		return sendJson(json)
 
 	}
 	catch(error) {
